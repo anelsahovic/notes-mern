c9525061e8e6b6293beb2b17b2d410f7ea2d0a23
Anel <anel.sahovic.bsc@gmail.com>
2025-04-01 10:02:10 +0200

    Database Note model setup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,21 @@
 import 'dotenv/config';
-
 import express from 'express';
+import NoteModel from './models/note';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/', async (req, res) => {
+  try {
+    const notes = await NoteModel.find().exec();
+    res.status(200).json(notes);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error });
+  }
+});
+
+app.use((req, res) => {
+  res.status(404).json({ error: 'Endpoint Not Found!' });
 });
 
 export default app;
