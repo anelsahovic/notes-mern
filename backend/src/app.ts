70a403f08717796be3df28d961d32eb779d5db34
Anel <anel.sahovic.bsc@gmail.com>
2025-04-03 12:00:49 +0200

    User authentication on backend(express-session)

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,32 @@
 import 'dotenv/config';
 import express, { NextFunction, Request, Response } from 'express';
 import notesRoutes from './routes/notes';
+import userRoutes from './routes/users';
 import createHttpError, { isHttpError } from 'http-errors';
+import session from 'express-session';
+import env from './utils/validateEnv';
+import MongoStore from 'connect-mongo';
 
 const app = express();
 
 app.use(express.json());
 
+app.use(
+  session({
+    secret: env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 6000 * 60 /* 6000 is one min, times how many mins you want*/,
+    },
+    rolling: true,
+    store: MongoStore.create({
+      mongoUrl: env.MONGODB_CONNECTION_STRING,
+    }),
+  })
+);
+
+app.use('/api/users', userRoutes);
 app.use('/api/notes', notesRoutes);
 
 app.use((req, res, next) => {
